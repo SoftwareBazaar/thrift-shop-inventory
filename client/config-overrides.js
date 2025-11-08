885343b331e60b353b86145cf90337d8c0b1cfb5
TradeTime Pro Developer <softwarebazaar.ke@gmail.com>
2025-11-08 17:30:10 +0300

    Polyfill vm for bcrypt build

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -5,7 +5,8 @@ module.exports = function override(config) {
     ...(config.resolve.fallback || {}),
     crypto: require.resolve('crypto-browserify'),
     stream: require.resolve('stream-browserify'),
-    buffer: require.resolve('buffer')
+    buffer: require.resolve('buffer'),
+    vm: require.resolve('vm-browserify')
   };
 
   config.plugins = (config.plugins || []).concat([
